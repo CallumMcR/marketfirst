ed3f5d30702caf3252015150808771bee11d1171
CallumMcR <p110134092@student.peterborough.ac.uk>
2023-01-09 14:47:47 +0000

    React-Router-DOM installed

---
+++ src/components/Index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function Index() {
+    return (
+        <div className="home">
+           
+        </div>
+    )
+}
+
+export default Index;
