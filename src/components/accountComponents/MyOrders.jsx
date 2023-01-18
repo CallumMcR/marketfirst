5ad27d71b663015923f45b772cc29448dc8dd8a1
CallumM <p110134092@student.peterborough.ac.uk>
2023-01-18 14:27:27 +0000

    Orders page accordion

--- src/components/accountComponents/MyOrders.jsx
+++ src/components/accountComponents/MyOrders.jsx
@@ -27,6 +27,8 @@ function CustomToggle({ children, eventKey }) {
 
 function MyOrders() {
 
+    const [orderedProducts, setOrderedProducts] = useState([{ orderID: "213131312", productName: "Nike Trainers", price: "20.00" }]);
+
     return (
         <div className="">
             <div className="text-center">
@@ -37,44 +39,56 @@ function MyOrders() {
 
                 </div>
                 <div className="col-8 container-custom-input">
-                    <Accordion defaultActiveKey="0">
-                        <Card style={{ display: "block" }}>
-                            <Card.Header>
-                                <CustomToggle eventKey="0" >
-                                    <div className="d-flex position-relative" style={{ flexGrow: 1 }}>
+                    {orderedProducts.map((product, index) => {
+                        return (
 
-                                        <img style={{ maxHeight: "100px" }} className='img-fluid img-thumbnail' src={pic} alt="product"></img>
+                            <Accordion defaultActiveKey="0" key={index}>
+                                <Card style={{ display: "block" }}>
+                                    <Card.Header>
+                                        <CustomToggle eventKey="0" >
+                                            <div className="d-flex position-relative" style={{ flexGrow: 1 }}>
 
-                                        <div className="container-fluid w-100">
-                                            <div className="row">
-                                                <div className="col-10 text-start">
-                                                    <div className="fs-5">
-                                                        Product Name
-                                                    </div>
-                                                    <div className="fs-5">
-                                                        £29.99
-                                                    </div>
-                                                    <div className="fs-5">
-                                                         Order No. #112312312
+                                                <img style={{ maxHeight: "100px" }} className='img-fluid img-thumbnail' src={pic} alt="product"></img>
+
+                                                <div className="container-fluid w-100">
+                                                    <div className="row">
+                                                        <div className="col-10 text-start">
+                                                            <div className="fs-5">
+                                                                {product.productName}
+                                                            </div>
+                                                            <div className="fs-5">
+                                                                £{product.price}
+                                                            </div>
+                                                            <div className="fs-5">
+                                                                Order No. #{product.orderID}
+                                                            </div>
+                                                        </div>
+                                                        <div className="col-2">
+                                                            <div className="text-end">
+                                                                <i class="bi bi-caret-down-fill"></i>
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                                <div className="col-2">
-                                                    <div className="text-end">
-                                                        <i class="bi bi-caret-down-fill"></i>
-                                                    </div>
-                                                </div>
+
                                             </div>
-                                        </div>
+                                        </CustomToggle>
+                                    </Card.Header>
+                                    <Accordion.Collapse eventKey="0">
+                                        <Card.Body>
+                                            Information about the product goes here
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
 
-                                    </div>
-                                </CustomToggle>
-                            </Card.Header>
-                            <Accordion.Collapse eventKey="0">
-                                <Card.Body>Hello! I'm the body</Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
+                            </Accordion>
 
-                    </Accordion>
+                        )
+
+
+
+                    })}
+
 
 
 
