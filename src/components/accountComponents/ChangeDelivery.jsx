286520703f22265c677bb35dacae52e935aa444a
CallumM <p110134092@student.peterborough.ac.uk>
2023-01-18 13:43:27 +0000

    Start to change delivery page

--- src/components/accountComponents/ChangeDelivery.jsx
+++ src/components/accountComponents/ChangeDelivery.jsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../css/login.css'
 import React, { useState } from 'react';
 
 
@@ -10,7 +11,29 @@ function ChangeDelivery() {
 
     return (
         <div className="">
-            Change delivery Details
+            <div className="text-center">
+                Change delivery details
+            </div>
+            <div className="row">
+                <div className="col-3">
+
+                </div>
+                <div className="col-6 container-custom-input">
+                    <input
+                        placeholder='First Name'>
+
+                    </input>
+                    <input
+                    placeholder='Address 1'>
+                    </input>
+                    <input
+                    placeholder='Address 2'>
+                    </input>
+                </div>
+                <div className="col-3">
+
+                </div>
+            </div>
         </div>
     )
 }
