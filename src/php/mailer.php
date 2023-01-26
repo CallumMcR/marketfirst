f9ff5c60cb7cfb38d3084573a870fd40e82b32e1
Callum McRoyall <callumbmcroyall@hotmail.com>
2023-01-26 15:30:14 +0000

    mailer update

--- src/php/mailer.php
+++ src/php/mailer.php
@@ -1,4 +1,45 @@
 <?php
+
+use PHPMailer/PHPMailer;
+use PHPMailer/PHPMailer/Exception;
+
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/STMP.php';
+
+
+if(isset($_POST['send'])){
+  $mail = new PHPMailer(true);
+
+  $mail ->isSTMP();
+  $mail->Host = 'stmp.gmail.com';
+  $mail->Username='p110134092@student.peterborough.ac.uk';
+  $mail->Password ='tlwuysrigspesqvj';
+  $mail->STMPSecure ='ssl';
+  $mail->Port =465;
+
+  $mail->setForm('p110134092@student.peterborough.ac.uk');
+
+  $mail->addAddress($_POST['email']);
+
+  $mail->isHTML(true);
+
+  $mail->Subject =$_POST["subject"];
+  $mail->Body =$_POST["message"];
+
+  $mail->send();
+
+  echo
+  "
+  <script>
+  alert('sent successfully')
+  </script>
+  "
+
+}
+
+
 // Multiple recipients
 $recipient = 'johny@example.com, sally@example.com'; // note the comma
 
