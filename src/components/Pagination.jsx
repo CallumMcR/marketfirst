0c8bd0e564897f33bb6b266a799001db2bd0c2e6
CallumMcR <p110134092@student.peterborough.ac.uk>
2023-04-18 16:50:22 +0100

    Removed spacing on pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import { useState } from 'react';
 import '../css/pagination.css';
-
 const Pagination = ({ productsPerPage, totalProducts, paginate }) => {
     const [currentPage, setCurrentPage] = useState(1);
     const pageNumbers = [];
-
     for (let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
         pageNumbers.push(i);
     }
     const maxPagesVisible = 10;
     const pagesToShow = pageNumbers.slice(currentPage - 1, currentPage + maxPagesVisible - 1);
-
-
     const handlePreviousPage = (newPage) => {
         setCurrentPage(newPage);
         paginate(newPage)
     };
-
     const handlePageChange = (newPage) => {
         setCurrentPage(newPage);
         paginate(newPage)
@@ -27,12 +22,9 @@ const Pagination = ({ productsPerPage, totalProducts, paginate }) => {
         setCurrentPage(newPage);
         paginate(newPage)
     };
-
     return (
         <nav>
-
             <div className="d-flex justify-content-start">
-
                 {currentPage !== 1 ?
                     <div className="paginationNumber-container" onClick={() => handlePreviousPage(currentPage - 1)}>
                         <div className="pagination-number">
@@ -46,8 +38,6 @@ const Pagination = ({ productsPerPage, totalProducts, paginate }) => {
                         </div>
                     </div>
                 }
-
-
                 {pagesToShow.map(number => (
                     <div className={`paginationNumber-container ${currentPage === number ? 'active-page' : ''}`} onClick={() => handlePageChange(number)} key={number}>
                         <div className="pagination-number">
@@ -55,8 +45,6 @@ const Pagination = ({ productsPerPage, totalProducts, paginate }) => {
                         </div>
                     </div>
                 ))}
-
-
                 {typeof pageNumbers[currentPage] !== 'undefined' ?
                     <div className="paginationNumber-container" onClick={() => handleNextPage(currentPage + 1)}>
                         <div className="pagination-number">
@@ -70,20 +58,8 @@ const Pagination = ({ productsPerPage, totalProducts, paginate }) => {
                         </div>
                     </div>
                 }
-
             </div>
-
-
-
-
-
-
-
-
-
         </nav >
-
     )
 }
-
 export default Pagination
