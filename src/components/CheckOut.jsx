a16837f6d6fe700ce98a80fb2bf545c19e4c0926
CallumMcR <p110134092@student.peterborough.ac.uk>
2023-05-02 13:28:58 +0100

    Checkout - Improved clarity

--- src/components/CheckOut.jsx
+++ src/components/CheckOut.jsx
@@ -14,10 +14,7 @@ import Footer from "./Footer"
 import Cookies from 'universal-cookie';
 
 function CheckOut() {
-
-
     const [basketTotal, setBasketTotal] = useState(0);
-
     const [basketItems, setBasketItems] = useState([]);
     const setTotalPrice = () => {
         let total = 0;
@@ -27,7 +24,6 @@ function CheckOut() {
         console.log("Total checkout", total);
         setBasketTotal(total);
     }
-
     useEffect(() => {
         const basketItemsFromStorage = JSON.parse(sessionStorage.getItem("basketData")) || [];
         setBasketItems(basketItemsFromStorage);
@@ -42,6 +38,36 @@ function CheckOut() {
     const [progressBar, setProgress] = useState(0);
 
     const [backClass, setBackClass] = useState(" disabled");
+
+    const handleProgressClick = (progressValue) => {
+        let newProgress = progressBar;
+
+        if (progressValue > progressBar) {
+            newProgress = progressBar + 1;
+            setProgress(newProgress);
+            $(".nConfirm" + newProgress).each(function () {
+                $(this).addClass("done");
+            });
+            var pBar = (progressValue / 2) * 100;
+            $(".pBar").css("width", `${pBar}%`);
+            if (progressValue == 2) {
+                $("#next").addClass("disabled");
+            }
+        } else if (progressValue < progressBar) {
+            $(".nConfirm" + progressBar).each(function () {
+                $(this).removeClass("done");
+            });
+            newProgress = progressBar - 1;
+            setProgress(newProgress);
+            var pBar = (progressValue / 2) * 100;
+            $(".pBar").css("width", `${pBar}%`);
+            if (progressValue == 0) {
+                setBackClass(" disabled");
+            }
+        }
+    };
+
+
 
     const progressBarChange = (state) => {
         if (state === "next") {
@@ -74,16 +100,12 @@ function CheckOut() {
         }
 
     }
-
-
     return (
         <div>
             <NavigationBar>
             </NavigationBar>
-
             <div className="row">
                 <div className="col-2">
-
                 </div>
                 <div className="col-8">
                     <div className="text-center">
@@ -92,7 +114,6 @@ function CheckOut() {
                         </div>
                     </div>
                     <div className="container d-flex justify-content-center">
-
                         <div className="progress-bar-container">
                             <div class="mainWrapper">
                                 <div class="statusBar">
@@ -114,160 +135,39 @@ function CheckOut() {
                         </div>
                     </div>
 
+                    {
+                        // Not logged in stage 0
+                        userID === undefined && progressBar === 0 &&
+                        <GuestStage0
+                            basketItems={basketItems}
+                            basketTotal={basketTotal}
+                            onProgressClick={handleProgressClick}
+                        />
 
-
-
-
-
-                    {/* State 0 - Logged in */}
+                    }
+                    {
+                        // No items in basket
+                        basketItems.length <= 0 &&
+                        <div className="container">
+                            <div className="text-center empty-cart">
+                                Your cart is empty
+                            </div>
+                            <div className="guest-header">
+                                <span className="browse-products">Browse products here</span>
+                            </div>
+                        </div>
+                    }
 
                     {
-
-
-                        userID != undefined && progressBar === 0 ?
-
-                            basketItems.length > 0 ?
-                                <div className="container">
-
-
-                                    <div className="justify-content-center d-flex">
-
-                                        {basketItems.map((product) => (
-                                            <div className="p-5" key={product.productID}>
-                                                <div className="card" style={{ border: "none" }}>
-                                                    <img src={require(`../PHP/images/products/${product.productID}/image1.png`)} alt="product" />
-                                                    <div className="card-body">
-                                                        <h5 className="card-title fs-1">{product.productName}</h5>
-
-                                                        <div className="d-flex">
-
-                                                            <div className="quantity-text px-4">
-                                                                {product.quantity}
-
-
-                                                            </div>
-                                                        </div>
-                                                        <div className="size-text">
-                                                            Size:{product.shoeSize}
-                                                        </div>
-                                                        <div className='price-text'>
-                                                            £{product.price * product.quantity}
-                                                        </div>
-
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        ))}
-
-
-
-                                    </div>
-
-                                    <div className="container border-bottom">
-                                        <div className="d-flex justify-content-around">
-                                            <div className=" subtotal-text">
-                                                Subtotal
-                                            </div>
-                                            <div className="subtotal-price">
-                                                £ {basketTotal}
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="container">
-                                        <div className="d-flex justify-content-center">
-                                            <div className="login-button mt-4">
-                                                Pay
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                :
-
-                                <div className="container">
-                                    <div className="text-center empty-cart">
-                                        Your cart is empty
-                                    </div>
-                                    <div className="guest-header">
-                                        <span className="browse-products">Browse products here</span>
-                                    </div>
-
-                                </div>
-
-                            // State 0 - Finish
-
-                            :
-
-                            /* State 1 - Not logged in*/
-
-                            basketItems.length > 0 ?
-                                <div className="container">
-
-                                    <div className="justify-content-center d-flex">
-
-                                        {basketItems.map((product) => (
-                                            <div className="p-5" key={product.productID}>
-                                                <div className="card" style={{ border: "none" }}>
-                                                    <img src={require(`../PHP/images/products/${product.productID}/image1.png`)} alt="product" />
-                                                    <div className="card-body">
-                                                        <h5 className="card-title fs-1">{product.productName}</h5>
-                                                        <div className="d-flex">
-                                                            <div className="quantity-text px-4">
-                                                                {product.quantity}
-                                                            </div>
-                                                        </div>
-                                                        <div className="size-text">
-                                                            Size:{product.shoeSize}
-                                                        </div>
-                                                        <div className='price-text'>
-                                                            £{product.price * product.quantity}
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        ))}
-
-
-
-                                    </div>
-
-                                    <div className="container border-bottom">
-                                        <div className="d-flex justify-content-around">
-                                            <div className=" subtotal-text">
-                                                Subtotal
-                                            </div>
-                                            <div className="subtotal-price">
-                                                £ {basketTotal}
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="container">
-                                        <div className="d-flex justify-content-center my-4">
-                                            <div className="login-button">
-                                                Login
-                                            </div>
-                                        </div>
-                                        <div className="guest-header">
-                                            <span>Or</span>
-                                        </div>
-                                        <div className="d-flex justify-content-center">
-                                            <div className="pay-guest">
-                                                Pay as a guest
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                :
-                                <div className="container">
-                                    <div className="text-center empty-cart">
-                                        Your cart is empty
-                                    </div>
-                                    <div className="guest-header">
-                                        <span className="browse-products">Browse products here</span>
-                                    </div>
-
-                                </div>
-                                // State 1 - not logged in finish
+                        // Logged in stage 0
+                        userID !== undefined && progressBar === 0 &&
+                        <UserStage0
+                            basketItems={basketItems}
+                            basketTotal={basketTotal}
+                            onProgressClick={handleProgressClick}
+                        />
                     }
+
 
 
 
@@ -278,7 +178,6 @@ function CheckOut() {
 
                 </div>
                 <div className="col-2">
-
                 </div>
             </div>
             <Footer></Footer>
@@ -287,3 +186,149 @@ function CheckOut() {
 }
 
 export default CheckOut;
+
+function GuestStage0({ basketItems, basketTotal, onProgressClick }) {
+    return (
+        basketItems.length > 0 ?
+            <div className="container">
+                <div className="justify-content-center d-flex">
+                    {basketItems.map((product) => (
+                        <div className="p-5" key={product.productID}>
+                            <div className="card" style={{ border: "none" }}>
+                                <img src={require(`../PHP/images/products/${product.productID}/image1.png`)} alt="product" />
+                                <div className="card-body">
+                                    <h5 className="card-title fs-1">{product.productName}</h5>
+                                    <div className="d-flex">
+                                        <div className="quantity-text px-4">
+                                            {product.quantity}
+                                        </div>
+                                    </div>
+                                    <div className="size-text">
+                                        Size:{product.shoeSize}
+                                    </div>
+                                    <div className='price-text'>
+                                        £{product.price * product.quantity}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className="container border-bottom">
+                    <div className="d-flex justify-content-around">
+                        <div className=" subtotal-text">
+                            Subtotal
+                        </div>
+                        <div className="subtotal-price">
+                            £ {basketTotal}
+                        </div>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="d-flex justify-content-center">
+                        <div className="login-button mt-4" onClick={() => onProgressClick(1)}>
+                            Pay
+                        </div>
+                    </div>
+                </div>
+            </div>
+            :
+            <div className="container">
+                <div className="text-center empty-cart">
+                    Your cart is empty
+                </div>
+                <div className="guest-header">
+                    <span className="browse-products">Browse products here</span>
+                </div>
+            </div>
+    );
+}
+
+function UserStage0({ basketItems, basketTotal, onProgressClick }) {
+    return (
+        <div className="container">
+            <div className="justify-content-center d-flex">
+                {basketItems.map((product) => (
+                    <div className="p-5" key={product.productID}>
+                        <div className="card" style={{ border: "none" }}>
+                            <img src={require(`../PHP/images/products/${product.productID}/image1.png`)} alt="product" />
+                            <div className="card-body">
+                                <h5 className="card-title fs-1">{product.productName}</h5>
+                                <div className="d-flex">
+                                    <div className="quantity-text px-4">
+                                        {product.quantity}
+                                    </div>
+                                </div>
+                                <div className="size-text">
+                                    Size:{product.shoeSize}
+                                </div>
+                                <div className='price-text'>
+                                    £{product.price * product.quantity}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="container border-bottom">
+                <div className="d-flex justify-content-around">
+                    <div className=" subtotal-text">
+                        Subtotal
+                    </div>
+                    <div className="subtotal-price">
+                        £ {basketTotal}
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="d-flex justify-content-center my-4">
+                    <div className="login-button">
+                        Login
+                    </div>
+                </div>
+                <div className="guest-header">
+                    <span>Or</span>
+                </div>
+                <div className="d-flex justify-content-center">
+                    <div className="pay-guest" onClick={() => onProgressClick(1)}>
+                        Pay as a guest
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function GuestStage1() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+                    test
+                </div>
+                <div className="col-6">
+                    test
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function UserStage1() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
